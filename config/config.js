cde9f3548f4db42037fa26067869aa6d8a5ff81c
Muriel <eesneider2118@gmail.com>
2025-03-04 14:40:46 -0500

    config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgresql://flutterDB_owner:npg_eWuQE6O5kSIN@ep-gentle-unit-a5gvqd2j-pooler.us-east-2.aws.neon.tech/flutterDB?sslmode=require',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
@@ -14,12 +14,12 @@ module.exports = {
   test: {
     username: 'root',
     password: null,
-    database: 'database_test',
+    database: 'flutterBackend',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgresql://flutterDB_owner:npg_eWuQE6O5kSIN@ep-gentle-unit-a5gvqd2j-pooler.us-east-2.aws.neon.tech/flutterDB?sslmode=require',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
